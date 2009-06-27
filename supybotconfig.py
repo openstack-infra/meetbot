c2928526bd7b4de3dc8d772a2e7fce117d852b39
Richard Darst <rkd@zgib.net>
2009-06-27 15:49:26 -0700

    add support for setting writer_map via supybot
    - we define our own WriterMap class to translate the config to/from a
      string.  This is the Right Way to integrate with the supybot registry.
    
    darcs-hash:20090627224926-82ea9-c1dfa4da4724e29d660382082a7ced0da1d4b5c1.gz

--- supybotconfig.py
+++ supybotconfig.py
@@ -4,10 +4,44 @@ import supybot.conf as conf
 import supybot.registry as registry
 
 import meeting
+import writers
+
 OriginalConfig = meeting.Config
 
 # The plugin group for configuration
 MeetBotConfigGroup = conf.registerPlugin('MeetBot')
+
+class WriterMap(registry.String):
+    """List of output formats to write.  This is a space-separated
+    list of 'WriterName:.ext' pairs.  WriterName must be from the
+    writers.py module, '.ext' must be a extension ending in a .
+    """
+    def set(self, s):
+        s = s.split()
+        writer_map = { }
+        for writer in s:
+            #from fitz import interact ; interact.interact()
+            writer, ext = writer.split(':')
+            if not hasattr(writers, writer):
+                raise ValueError("Writer name not found: %s"%writer)
+            if len(ext) < 2 or ext[0] != '.':
+                raise ValueError("Extension must start with '.' and have "
+                                 "at least one more character.")
+            writer_map[ext] = getattr(writers, writer)
+        self.setValue(writer_map)
+    def setValue(self, writer_map):
+        for e, w in writer_map.iteritems():
+            if not hasattr(w, "format"):
+                raise ValueError("Writer %s must have method .format()"%
+                                 w.__class__.__name__)
+            self.value = writer_map
+    def __str__(self):
+        writers_string = [ ]
+        for ext, w in self.value.iteritems():
+            name = w.__class__.__name__
+            writers_string.append("%s:%s"%(name, ext))
+        return " ".join(writers_string)
+
 
 class SupybotConfig(object):
     def __init__(self, M):
@@ -21,6 +55,8 @@ class SupybotConfig(object):
         if attrname in settable_attributes:
             value = self.__C.M._registryValue(attrname,
                                               channel=self.__C.M.channel)
+            if not isinstance(value, (str, unicode)):
+                return value
             if value != '.':
                 value = value.replace('\\n', '\n')
                 return value
@@ -65,6 +101,14 @@ if (use_supybot_config.value and
         conf.registerChannelValue(MeetBotConfigGroup, attrname,
                                   registry.String(attr,""))
         settable_attributes.append(attrname)
+
+    # writer_map
+    if 'writer_map' in MeetBotConfigGroup._children:
+        MeetBotConfigGroup.unregister('writer_map')
+    conf.registerChannelValue(MeetBotConfigGroup, 'writer_map',
+                      WriterMap(OriginalConfig.writer_map, ""))
+    settable_attributes.append('writer_map')
+
 
     # Here is where the real proxying occurs.
     meeting.Config = SupybotConfig
