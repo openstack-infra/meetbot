af4d46e7d99c7f9146bb79468b2be3db1cedbd2d
Richard Darst <rkd@zgib.net>
2009-07-06 20:05:16 -0700

    Add doc/ directory and example config file
    - moved Manual.txt into this directory
    - added a minimal example file meetingLocalConfig-example.py
    
    darcs-hash:20090707030516-82ea9-a3ec9c0b441e7a9e1bbcb63872427eeb2ec2a498.gz

---
+++ doc/meetingLocalConfig-example.py
@@ -0,0 +1,19 @@
+# Richard Darst, July 2009
+#
+# Minimal meetingLocalConfig.py
+#
+# This file is released into the public domain, or released under the
+# supybot license in areas where releasing into the public domain is
+# not possible.
+#
+
+class Config(object):
+    # These are "required":
+    logFileDir = '/home/richard/meetbot/'
+    logUrlPrefix = 'http://rkd.zgib.net/meetbot/'
+
+    # These, you might want to change:
+    #MeetBotInfoURL = 'http://wiki.debian.org/MeetBot'
+    #filenamePattern = '%(channel)s/%%Y/%(channel)s.%%F-%%H.%%M'
+
+
