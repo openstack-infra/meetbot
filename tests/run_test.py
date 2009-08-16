9fcebd938ff8d316ccea74d2af8ad01a6e16371c
Richard Darst <rkd@zgib.net>
2009-08-15 19:18:54 -0700

    Update test runner script to actually work
    - Now doesn't ignore the status output of the supybot-based tests.
    - Uses unittest framework (but not that well)
    - Overall, works better now.  But testing is still tricky, this is far
      from perfect.
    
    darcs-hash:20090816021854-82ea9-2dddc3860357fe20ea878d53c005e37171695361.gz

--- tests/run_test.py
+++ tests/run_test.py
@@ -2,26 +2,48 @@
 
 import os
 import sys
+import unittest
 
-run_tests = True
-os.chdir(os.path.join(os.path.dirname(__file__), '.'))
+runing_tests = True
 
-print sys.path
-sys.path.insert(0, "..")
+class MeetBotTest(unittest.TestCase):
 
-sys.argv[1:] = ["replay", "test-script-1.log.txt"]
-execfile("../meeting.py")
+    def test_replay(self):
+        """Replay of a meeting, using __meeting__.
+        """
+        sys.argv[1:] = ["replay", "test-script-1.log.txt"]
+        sys.path.insert(0, "..")
+        try:
+            execfile("../meeting.py", globals())
+        finally:
+            del sys.path[0]
 
-del sys.path[0]
+    def test_supybottests(self):
+        """Test by sending input to supybot, check responses.
+
+        Uses the external supybot-test command.  Unfortunantly, that
+        doesn't have a useful status code, so I need to parse the
+        output.
+        """
+        os.symlink("..", "MeetBot")
+        try:
+            output = os.popen("supybot-test ./MeetBot 2>&1").read()
+            print output
+            assert 'FAILED' not in output, "supybot-based tests failed."
+        finally:
+            os.unlink("MeetBot")
 
 
-# Supybot-based tests
 
-os.symlink("..", "MeetBot")
-try:
-    sys.argv[1:] = ["./MeetBot"]
-    execfile("/usr/bin/supybot-test")
-finally:
-    os.unlink("MeetBot")
+if __name__ == '__main__':
+    os.chdir(os.path.join(os.path.dirname(__file__), '.'))
+    unittest.main()
+
+
+
+
+
+
+
 
 
