319b5547a86fb2cd4a69bef6babd98bd26eb460f
Garrett Holmstrom <gholms@fedoraproject.org>
2012-11-17 23:43:53 -0800

    Fix setup.py package names
    It looks like MeetBot's setup.py never got updated when the plugin's
    name changed back to MeetBot. The attached patch fixes that so
    setup.py starts working correctly again. Fixes bug 1080268.
    
    Change-Id: I76ad5e5c66f1f44aa260c76ed7392887fc610839

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(name='MeetBot',
       description='IRC Meeting Helper',
       version='0.1.4',
-      packages=['supybot.plugins.Meeting',
+      packages=['supybot.plugins.MeetBot',
                 'ircmeeting'],
-      package_dir={'supybot.plugins.Meeting':'Meeting'},
+      package_dir={'supybot.plugins.MeetBot':'MeetBot'},
       package_data={'ircmeeting':['*.html', '*.txt', '*.css']},
       author="Richard Darst",
       author_email="rkd@zgib.net"
